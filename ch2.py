84d9409c9bad4c45bea25865f263e735e6324e86
H S <hannah.snyderburn@gmail.com>
2024-03-07 11:15:56 -0500

    i had to make environments to run numpy got a p2/p1 equation

--- ch2.py
+++ ch2.py
@@ -11,9 +11,17 @@ import numpy as np
 #(not parallel and perpendicular to shock wave, as is usally taken in most standard shock wave derivations)
 
 #following equations for oblique shock waves:
-#upstream = 1, downstream = 2
+#upstream = 1, downstream = 2, beta = wave angle, M = mach number
+#assumptions: calorically perfect gas - constant speciic heats
+#M1 goes to infinity, M1^2 * sin^2(beta) >> 1. So M1^2 * sin^2(beta) - 1 = M1^2 * sin^2(beta)
+
+M1 = 8
+gamma = 1.4
+beta = 12
+p2_p1 = 1 + np.divide(2*gamma,gamma+1) * ((M1**2)*(np.sin(beta)**2))
 
 
-t = 
-rho =
-basicCont = rho/t + np.gradient(rho*u)
+print(p2_p1)
+# t = 1
+# rho =1
+# basicCont = rho/t + np.gradient(rho*u)
