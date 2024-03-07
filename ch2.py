d36d7859a631e9db82b4fd6c5d212f055c9f2009
H S <hannah.snyderburn@gmail.com>
2024-03-07 10:12:03 -0500

    i began ch.2

---
+++ ch2.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+#Problem 2.1
+
+#basic continuity, momentum, energy equations for flow across oblique shock waves (ex 4)
+#deriv equations 2.6 and 2.8
+
+#is there a python capablity to derive equations for us? 
+
+#u2 and v2 are velocity components behind shck parallel and perpendicular to the upstream velocity.
+#(not parallel and perpendicular to shock wave, as is usally taken in most standard shock wave derivations)
+
+basicCont = 
